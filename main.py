faa4503a93d2500c18db252598e53d40f9c2207d
suyash-more <suyashmore789@gmail.com>
2022-01-13 21:41:29 +0530

    initial push

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import json
+import urllib
+import webapp2
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+    def post(self):
+        pincode = self.request.get('zipCode')
+        url = "https://api.postalpincode.in/pincode/"+ pincode
+        data = urllib.urlopen(url).read()
+        data = json.loads(data)
+        post_office = data[0]['PostOffice'][0]['State']
+        name = data[0]['PostOffice'][0]['Name']
+        block = data[0]['PostOffice'][0]['Block']
+        district = data[0]['PostOffice'][0]['District']
+        template_values = {
+            "post_office": post_office,
+            "name": name,
+            "block": block,
+            "district": district
+        }
+        path = os.path.join(os.path.dirname(__file__), 'results.html')
+        self.response.out.write(template.render(path, template_values))
+        
+        
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
