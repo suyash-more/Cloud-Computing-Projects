7cf209762ee4584430615ec24e7e045355f1c672
suyash-more <suyashmore789@gmail.com>
2022-01-29 17:39:21 +0530

    code changes in the index file

--- Deploy-Web-Application-as-PaaS/react-app/src/App.js
+++ Deploy-Web-Application-as-PaaS/react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first React Application
         </p>
         <a
           className="App-link"
