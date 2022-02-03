9c0948a53cbefc14a2c7bd6391289126d103a2b6
suyash-more <suyashmore789@gmail.com>
2022-02-03 10:13:49 +0530

    error checks updated

--- Web-Application-using-Gcloud-SDK/main.py
+++ Web-Application-using-Gcloud-SDK/main.py
@@ -12,6 +12,12 @@ class MainPage(webapp2.RequestHandler):
 
     def post(self):
         pincode = self.request.get('zipCode')
+        if not pincode.isnumeric() or not len(pincode)==6:
+            template_values = {
+                "error" : "Incorrect Pin Code (String / False Code entered)"
+            }
+            path = os.path.join(os.path.dirname(__file__), 'index.html')
+            return self.response.out.write(template.render(path, template_values))
         url = "https://api.postalpincode.in/pincode/"+ pincode
         data = urllib.urlopen(url).read()
         data = json.loads(data)
