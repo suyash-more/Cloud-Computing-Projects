cbf956a6fb426bc34274d140a26f8d562e7795a2
suyash-more <suyashmore789@gmail.com>
2022-05-13 17:02:03 +0530

    update on post office finder

--- Web-Application-using-Gcloud-SDK/main.py
+++ Web-Application-using-Gcloud-SDK/main.py
@@ -4,6 +4,7 @@ import urllib
 import webapp2
 from google.appengine.ext.webapp import template
 
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         template_values = {}
@@ -12,27 +13,32 @@ class MainPage(webapp2.RequestHandler):
 
     def post(self):
         pincode = self.request.get('zipCode')
-        if not pincode.isnumeric() or not len(pincode)==6:
+        if not pincode.isnumeric() or not len(pincode) == 6:
             template_values = {
-                "error" : "Incorrect Pin Code (String / False Code entered)"
+                "error": "Incorrect Pin Code (String / False Code entered)"
             }
             path = os.path.join(os.path.dirname(__file__), 'index.html')
             return self.response.out.write(template.render(path, template_values))
-        url = "https://api.postalpincode.in/pincode/"+ pincode
+        url = "https://api.postalpincode.in/pincode/" + pincode
         data = urllib.urlopen(url).read()
         data = json.loads(data)
-        post_office = data[0]['PostOffice'][0]['State']
-        name = data[0]['PostOffice'][0]['Name']
-        block = data[0]['PostOffice'][0]['Block']
-        district = data[0]['PostOffice'][0]['District']
-        template_values = {
-            "post_office": post_office,
-            "name": name,
-            "block": block,
-            "district": district
-        }
-        path = os.path.join(os.path.dirname(__file__), 'results.html')
-        self.response.out.write(template.render(path, template_values))
-        
-        
+        if(data[0]['Status'] == 'Success'):
+            post_office = data[0]['PostOffice'][0]['State']
+            name = data[0]['PostOffice'][0]['Name']
+            block = data[0]['PostOffice'][0]['Block']
+            district = data[0]['PostOffice'][0]['District']
+            template_values = {
+                "post_office": post_office,
+                "name": name,
+                "block": block,
+                "district": district
+            }
+            path = os.path.join(os.path.dirname(__file__), 'results.html')
+            self.response.out.write(template.render(path, template_values))
+        else:
+            template_values = {}
+            path = os.path.join(os.path.dirname(__file__), 'error.html')
+            self.response.out.write(template.render(path, template_values))
+
+
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
